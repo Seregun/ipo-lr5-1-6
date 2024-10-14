26880cdf809db6cf5514999991141aa1f291383f
Seregun <seregunmgp@gmail.com>
2024-10-14 18:46:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+with open('text.txt', 'r') as file: # Открытие файла text.txt для чтения
+    lines = file.readlines()  # Чтение всех строк из файла и сохранение их в переменной lines
+with open('output.txt', 'w') as output_file: # Открытие нового файла output.txt для записи
+    for i, line in enumerate(lines, 1):  # Использование функции enumerate для получения номера строки
+        output_line = f"{i}: {line}"  # Добавление номера строки перед каждой строкой
+        output_file.write(output_line)  # Записывание строки в новый файл
+        print(output_line)  # Вывод результата
