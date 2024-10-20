cad24ea83308143eea5a0704ff82dedd239b0dc6
Seregun <seregunmgp@gmail.com>
2024-10-20 20:58:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open('text.txt', 'r') as file: # Открытие файла text.txt для чтения
+with open('E:\\Python\\lr5_6\\text.txt', 'r') as file: # Открытие файла text.txt (указать путь к файлу) для чтения
     lines = file.readlines()  # Чтение всех строк из файла и сохранение их в переменной lines
 with open('output.txt', 'w') as output_file: # Открытие нового файла output.txt для записи
     for i, line in enumerate(lines, 1):  # Использование функции enumerate для получения номера строки
