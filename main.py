92a15ecde1dd3af807ed42d9c82f37908c72c0e9
Seregun <seregunmgp@gmail.com>
2024-10-21 15:02:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 with open('E:\\Python\\lr5_6\\text.txt', 'r') as file: # Открытие файла text.txt (указать путь к файлу) для чтения
     lines = file.readlines()  # Чтение всех строк из файла и сохранение их в переменной lines
-with open('output.txt', 'w') as output_file: # Открытие нового файла output.txt для записи
+with open('E:\\Python\\lr5_6\\output.txt', 'w') as output_file: # Открытие нового файла output.txt для записи
     for i, line in enumerate(lines, 1):  # Использование функции enumerate для получения номера строки
         output_line = f"{i}: {line}"  # Добавление номера строки перед каждой строкой
         output_file.write(output_line)  # Записывание строки в новый файл
